11148f3b495055ce33904ae200ae9d2e41b1bef6
Kyle <kyle.adams276@gmail.com>
2017-01-25 08:24:44 -0700

    Initial Commit

---
+++ FirstProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstProject
+//
+//  Created by Adams, Kyle on 1/25/17.
+//  Copyright © 2017 Adams, Kyle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
